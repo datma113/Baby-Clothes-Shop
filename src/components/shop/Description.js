e07596d60f0ed9cff79317726ce56cdfa288dfda
datma <datma113112111@gmail.com>
2021-05-25 11:03:59 +0700

    fix product detail...

--- src/components/shop/Description.js
+++ src/components/shop/Description.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
 
 import { useSelector } from "react-redux";
 
 const Description = () => {
-     const product = useSelector((state) => state.getProductByID);
-     return (
-          <div className="container" style={{marginTop:"10rem"}}>
-               <h1>Description</h1>
-               <p> {product.longDescription} </p>
-          </div>
-     )
-}
+    const product = useSelector((state) => state.getProductByID);
+    return (
+        <div className="container" style={{ marginTop: "10rem" }}>
+            <div style={{ marginBottom: `2rem` }}>
+                <span className="title-in-desc">Mô tả chi tiết:</span>
+                <p> {product.longDescription} </p>
+            </div>
 
-export default Description
+            <div>
+                <span className="title-in-desc">Xuất xứ:</span>
+                <p> {product.origin} </p>
+            </div>
+            <div>
+                <span className="title-in-desc">Chất liệu:</span>
+                <p> {product.material} </p>
+            </div>
+        </div>
+    );
+};
+
+export default Description;
